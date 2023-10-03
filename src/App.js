86c307d2ad6f954cfcd1e74daa0d7ca5f01b4f16
David <daviddwiyanto79@gmail.com>
2023-10-03 17:39:26 +0700

    feat(all)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import {
 
 import Login from './pages/Login';
 import Register from "./pages/Register";
-import Dashboard from "./pages/authPages/dashboard";
+import Dashboard from "./pages/authPages/NavbarMenu/Dashboard/dashboard";
+import NotFound404 from "./url/NotFound404";
+import ManajemenData from "./pages/authPages/NavbarMenu/ManajemenData/ManajemenData";
+import ManajemenUser from "./pages/authPages/NavbarMenu/ManajemenUser/ManajemenUser";
+import Projek from "./pages/authPages/NavbarMenu/Projek/Projek";
+import Laporan from "./pages/authPages/NavbarMenu/Laporan/Laporan";
+import Kalkulasi from "./pages/authPages/NavbarMenu/Kalkulasi/Kalkulasi";
 
 function App() {
   return (
@@ -16,6 +22,12 @@ function App() {
             <Route path="/" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/404" element={<NotFound404 />} />
+            <Route path="/manajemen-data" element={<ManajemenData />} />
+            <Route path="/manajemen-user" element={<ManajemenUser />} />
+            <Route path="/projek" element={<Projek />} />
+            <Route path="/laporan" element={<Laporan />} />
+            <Route path="/kalkulasi" element={<Kalkulasi />} />
         </Routes>
       </BrowserRouter>
     </div>
