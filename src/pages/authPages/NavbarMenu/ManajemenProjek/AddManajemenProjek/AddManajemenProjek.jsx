18e1c23642cf597b93efd9aef6c84b9dee6a64f4
David <daviddwiyanto79@gmail.com>
2023-10-11 12:30:12 +0700

    feat (form tambah projek)

--- src/pages/authPages/NavbarMenu/ManajemenProjek/AddManajemenProjek/AddManajemenProjek.jsx
+++ src/pages/authPages/NavbarMenu/ManajemenProjek/AddManajemenProjek/AddManajemenProjek.jsx
@@ -3,77 +3,35 @@ import Bottom from '../../../../../components/BottomBar/Bottom'
 import Navbar from '../../../../../components/Navbar/Navbar'
 import { PhotoIcon } from '@heroicons/react/24/solid'
 import { Fragment, useState } from 'react'
-import { Listbox, Transition } from '@headlessui/react'
+import { Combobox, Transition } from '@headlessui/react'
 import { CheckIcon, ChevronDownIcon } from '@heroicons/react/20/solid'
-import classNames from 'classnames';
 
-
-const people = [
-    {
-      id: 1,
-      name: 'Wade Cooper',
-      avatar:
-        'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-      id: 2,
-      name: 'Arlene Mccoy',
-      avatar:
-        'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-      id: 3,
-      name: 'Devon Webb',
-      avatar:
-        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80',
-    },
-    {
-      id: 4,
-      name: 'David Dwiyanto',
-      avatar:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-      id: 5,
-      name: 'Tanya Fox',
-      avatar:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-      id: 6,
-      name: 'Hellen Schmidt',
-      avatar:
-        'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-      id: 7,
-      name: 'Caroline Schultz',
-      avatar:
-        'https://images.unsplash.com/photo-1568409938619-12e139227838?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-      id: 8,
-      name: 'Mason Heaney',
-      avatar:
-        'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-      id: 9,
-      name: 'Claudie Smitham',
-      avatar:
-        'https://images.unsplash.com/photo-1584486520270-19eca1efcce5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-      id: 10,
-      name: 'Emil Schaefer',
-      avatar:
-        'https://images.unsplash.com/photo-1561505457-3bcad021f8ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
+  const people = [
+    { id: 1, name: 'Wade Cooper' },
+    { id: 2, name: 'Arlene Mccoy' },
+    { id: 3, name: 'Devon Webb' },
+    { id: 4, name: 'Tom Cook' },
+    { id: 5, name: 'Tanya Fox' },
+    { id: 6, name: 'Hellen Schmidt' },
   ]
 
 
 const AddManajemenProjek = () => {
-    const [selected, setSelected] = useState(people[3])
+
+  const [selected, setSelected] = useState(people[0])
+  const [query, setQuery] = useState('')
+
+  const filteredPeople =
+    query === ''
+      ? people
+      : people.filter((person) =>
+          person.name
+            .toLowerCase()
+            .replace(/\s+/g, '')
+            .includes(query.toLowerCase().replace(/\s+/g, ''))
+        )
+
+
   return (
      <div className="min-h-full">
       <Navbar />
@@ -114,73 +72,6 @@ const AddManajemenProjek = () => {
               </div>
             </div>
                 </div>
-                <div class="">
-                <Listbox value={selected} onChange={setSelected}>
-                        {({ open }) => (
-                            <>
-                            <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900 mt-6">Penanggung Jawab</Listbox.Label>
-                            <div className="relative mt-2 sm:max-w-none">
-                                <Listbox.Button className="relative w-full cursor-default rounded-md bg-white mb-1 py-2 pl-3 pr-5 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
-                                <span className="flex items-center">
-                                    <img src={selected.avatar} alt="" className="h-5 w-5 flex-shrink-0 rounded-full" />
-                                    <span className="ml-3 block truncate">{selected.name}</span>
-                                </span>
-                                <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-                                    <ChevronDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                                </span>
-                                </Listbox.Button>
-
-                                <Transition
-                                show={open}
-                                as={Fragment}
-                                leave="transition ease-in duration-100"
-                                leaveFrom="opacity-100"
-                                leaveTo="opacity-0"
-                                >
-                                <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                                    {people.map((person) => (
-                                    <Listbox.Option
-                                        key={person.id}
-                                        className={({ active }) =>
-                                        classNames(
-                                            active ? 'bg-indigo-600 text-white' : 'text-gray-900',
-                                            'relative cursor-default select-none py-2 pl-3 pr-9'
-                                        )
-                                        }
-                                        value={person}
-                                    >
-                                        {({ selected, active }) => (
-                                        <>
-                                            <div className="flex items-center">
-                                            <img src={person.avatar} alt="" className="h-5 w-5 flex-shrink-0 rounded-full" />
-                                            <span
-                                                className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}
-                                            >
-                                                {person.name}
-                                            </span>
-                                            </div>
-
-                                            {selected ? (
-                                            <span
-                                                className={classNames(
-                                                active ? 'text-white' : 'text-indigo-600',
-                                                'absolute inset-y-0 right-0 flex items-center pr-4'
-                                                )}
-                                            >
-                                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                            </span>
-                                            ) : null}
-                                        </>
-                                        )}
-                                    </Listbox.Option>
-                                    ))}
-                                </Listbox.Options>
-                                </Transition>
-                            </div>
-                            </>
-                        )}
-                        </Listbox> 
-                </div>
             </div>
                 <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <div className="sm:col-span-4">
@@ -210,7 +101,7 @@ const AddManajemenProjek = () => {
                     </label>
                     <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
                         <div className="text-center">
-                        <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+                        <PhotoIcon className="mx-auto h-44 w-12 text-gray-300" aria-hidden="true" />
                         <div className="mt-4 flex text-sm leading-6 text-gray-600">
                             <label
                             htmlFor="file-upload"
@@ -226,19 +117,101 @@ const AddManajemenProjek = () => {
                     </div>
                     </div>
                 </div>
-            
+               
+              <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div class="sm:col-span-4">
+                    <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
+                        Penanggung Jawab
+                    </label>
+                <Combobox value={selected} onChange={setSelected}>
+                  <div className="relative mt-2">
+                    <div className="relative w-full cursor-default overflow-hidden bg-white text-left focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible focus-visible:ring-offset-2 focus-visible:ring-offset-1-300 sm:text-sm">
+                      <Combobox.Input
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        displayValue={(person) => person.name}
+                        onChange={(event) => setQuery(event.target.value)}
+                      />
+                      <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+                        <ChevronDownIcon
+                          className="h-5 w-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                      </Combobox.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      leave="transition ease-in duration-100"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                      afterLeave={() => setQuery('')}
+                    >
+                      <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                        {filteredPeople.length === 0 && query !== '' ? (
+                          <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                            Nothing found.
+                          </div>
+                        ) : (
+                          filteredPeople.map((person) => (
+                            <Combobox.Option
+                              key={person.id}
+                              className={({ active }) =>
+                                `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                  active ? 'bg-indigo-500 text-white' : 'text-gray-900'
+                                }`
+                              }
+                              value={person}
+                            >
+                              {({ selected, active }) => (
+                                <>
+                                  <span
+                                    className={`block truncate ${
+                                      selected ? 'font-medium' : 'font-normal'
+                                    }`}
+                                  >
+                                    {person.name}
+                                  </span>
+                                  {selected ? (
+                                    <span
+                                      className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                        active ? 'text-white' : 'text-teal-600'
+                                      }`}
+                                    >
+                                      <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                    </span>
+                                  ) : null}
+                                </>
+                              )}
+                            </Combobox.Option>
+                          ))
+                        )}
+                      </Combobox.Options>
+                    </Transition>
+                    <p className="mt-3 text-sm leading-6 text-gray-600">Pilih penanggung jawab dari users untuk mengatur projeknya.</p>
+                  </div>
+                </Combobox>
+                </div>
+
+                <div class="sm:col-span-2">
+                  <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Label</label>
+                  <div class="mt-2">
+                    <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </input>
+                    <p className="mt-3 ml-1 text-sm leading-6 text-gray-600">Label akan muncul di halaman projek sebagai tagline.</p>
+                  </div>
+                </div>
+              </div>
 
 
 
+                </div>
             <div className="mt-6 flex items-center justify-end gap-x-6">
                 <button
                 type="submit"
                 className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                Tambahkan
+                 Tambahkan
                 </button>
             </div>
-                </div>
              </div>
 
          </form>
