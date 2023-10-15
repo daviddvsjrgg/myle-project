16c4f11590ff600b570bdd66d1ad428fa7aa4475
David <daviddwiyanto79@gmail.com>
2023-10-15 13:00:12 +0700

    fix (desain button Tambahkan)

--- src/pages/authPages/NavbarMenu/Personal/AddPersonal/AddPersonal.jsx
+++ src/pages/authPages/NavbarMenu/Personal/AddPersonal/AddPersonal.jsx
@@ -106,16 +106,16 @@ const AddPersonal = () => {
 
 
                 </div>
-            <div className="mt-6 flex items-center justify-end gap-x-6">
-                <button
-                type="submit"
-                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                 Tambahkan
-                </button>
-            </div>
              </div>
 
+              <div className="mt-6 flex items-center justify-end gap-x-6">
+                  <button
+                  type="submit"
+                  className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                  Tambahkan
+                  </button>
+              </div>
          </form>
         </div>
       </main>
