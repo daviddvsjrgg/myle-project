d35e9ad73d75bc29b083cfc02f52ac88cc7469bc
David <daviddwiyanto79@gmail.com>
2023-11-19 15:41:09 +0700

    Validasi Login

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import {  createUserWithEmailAndPassword  } from 'firebase/auth';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../config/firebase/firebase';
 
 const Register = () => {
