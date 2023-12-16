70d4e7b50635efce5d1b77bef7538b1e6539709f
David <daviddwiyanto79@gmail.com>
2023-12-16 17:41:17 +0700

    fix(profile)

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -60,7 +60,7 @@ const Register = () => {
                     emailUser: user.email,
                     roleUser: "user",
                     imageUser: "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.webp",
-                    positionUser: ""
+                    positionUser: "Belum ada jabatan"
                   });
                   console.log("Document written with ID: ", docRef.id);
                   navigate('/');
